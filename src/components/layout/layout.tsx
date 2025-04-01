2650c7d9b124272873c8047d79d0c7d3f3615dcc
Viviana Camargo <camargo5256@gmail.com>
2025-04-01 13:25:05 -0400

    Add error page for users that are not signed in

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -2,8 +2,16 @@
 import { ReactNode } from "react";
 import Navbar from "./navbar/navbar";
 import Footer from "./footer/footer";
+import { usePathname } from "next/navigation";
+
+const SECURE_PATHS = ["/", "/orders", "/schedule", "/recipes"];
 
 const Layout = ({ children }: { children: ReactNode }) => {
+	const pathname = usePathname();
+
+	if (!SECURE_PATHS.includes(pathname)) {
+		return <>{children}</>;
+	}
 	return (
 		<>
 			<Navbar />
--- src/components/pageError/pageError.tsx
+++ src/components/pageError/pageError.tsx
@@ -1,6 +1,15 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+import Button from "../button/button";
+
 const PageError = () => {
+	const router = useRouter();
+
+	const onReturnHomeClick = () => {
+		router.push("/");
+	};
+
 	return (
 		<div>
 			<h3>Page Error</h3>
@@ -9,6 +18,7 @@ const PageError = () => {
 					"You don't have access to this page. To gain access, you can sign up or log in."
 				}
 			</p>
+			<Button caption="Return to home" onClick={onReturnHomeClick} />
 		</div>
 	);
 };
